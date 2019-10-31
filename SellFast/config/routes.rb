f09a6f8bcc3c2e2dc66ad1f97c71476326f03e97
Ericlui409 <luieric@berkeley.edu>
2019-10-30 18:25:48 -0700

    added resource routes

--- SellFast/config/routes.rb
+++ SellFast/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'orders/new'
-  get 'items/new'
-  get 'users/new'
+  resources :items
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
