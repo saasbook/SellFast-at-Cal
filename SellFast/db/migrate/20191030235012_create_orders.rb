ec8545e80f6320d93a2fca1fe5797692adbd1f10
Kyle Schweizer <kschweizer@berkeley.edu>
2019-10-30 18:14:24 -0700

    added orders to migrations

--- SellFast/db/migrate/20191030235012_create_orders.rb
+++ SellFast/db/migrate/20191030235012_create_orders.rb
@@ -1,7 +1,12 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-
+      t.string :item
+      t.string :seller
+      t.string :buyer
+      t.string :location
+      t.float :amount
+      t.string :status
       t.timestamps
     end
   end
