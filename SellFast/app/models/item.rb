796f4a0f2945a3b3e7ad8c299dde52be992d1f56
Ericlui409 <luieric@berkeley.edu>
2019-11-09 00:01:07 -0800

    added expiry to item

--- SellFast/app/models/item.rb
+++ SellFast/app/models/item.rb
@@ -1,10 +1,21 @@
 class Item < ApplicationRecord
 	validates :name, :current_price, :status, presence: true
-	
+	after_find :update_status
+
 	has_many :bids
 	belongs_to :seller, class_name: 'User'
 
 	def self.all_status
-		%w(:BIDDING, :SOLD)
+		%w(:BIDDING, :SOLD, :EXPIRED)
 	end
+
+	def update_status
+		if self.status == :BIDDING
+			if (self.time_listed + 1/(24*60*60)) <= DateTime.now
+				self.status = :EXPIRED
+				self.save!
+			end
+		end
+	end
+	
 end
