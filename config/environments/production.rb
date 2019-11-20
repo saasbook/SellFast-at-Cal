b90f530e6595801f1660127f40edcd42a1c3289f
Ericlui409 <luieric@berkeley.edu>
2019-11-19 22:53:54 -0800

    edited prod configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,9 +67,9 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.default_url_options = { :host => 'https://sellfast.herokuapp.com' }
-  ActionMailer::Base.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
 
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
     domain: "heroku.com",
     port: 25,
